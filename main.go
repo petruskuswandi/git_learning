754bc51337de5affdff0392616183edaf05849d7
Petrus Kuswandi <petruskuswandi1@gmail.com>
2023-02-25 18:58:21 +0700

    Fixing git stash

--- main.go
+++ main.go
@@ -6,5 +6,10 @@ func main() {
 	fmt.Println("Hello World")
 	fmt.Println("A")
 	fmt.Println("B")
+<<<<<<< Updated upstream
 	fmt.Println("F")
+=======
+	fmt.Println("C")
+	fmt.Println("D")
+>>>>>>> Stashed changes
 }
